d555a3c49cb96b003c55bbc96609cdd9673a9c02
Sebastian Vargas Ramirez <AlpharGM@hotmail.com>
2022-06-30 17:04:57 -0500

    Funcional

--- src/heroes/routes/HeroesRoutes.jsx
+++ src/heroes/routes/HeroesRoutes.jsx
@@ -1,22 +1,21 @@
-import { Route, Routes, Navigate } from "react-router-dom";
-
+import { Navigate, Route, Routes } from "react-router-dom";
 import { Navbar } from "../../ui";
-import { MarvelPage, DcPage, SearchPage, HeroPage } from "../pages";
+import { DcPage, HeroPage, MarvelPage, SearchPage } from "../pages";
 
 export const HeroesRoutes = () => {
   return (
     <>
-        <Navbar />
-      <div className="container">
+      <Navbar />
 
+      <div className='container'>
         <Routes>
-          <Route path='/Marvel' element={<MarvelPage />} />
-          <Route path='/DC' element={<DcPage />} />
-          <Route path='/Search' element={<SearchPage />} />
-          <Route path='/Hero' element={<HeroPage />} />
+          <Route path='marvel' element={<MarvelPage />} />
+          <Route path='dc' element={<DcPage />} />
 
+          <Route path='search' element={<SearchPage />} />
+          <Route path='hero/:id' element={<HeroPage />} />
 
-          <Route path='/' element={<Navigate to='/Marvel' />} />
+          <Route path='/' element={<Navigate to='/marvel' />} />
         </Routes>
       </div>
     </>
